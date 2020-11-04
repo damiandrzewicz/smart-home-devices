d63e223037f9d0746888bb9d918625f7b9a4a09c
Damian <damian.drzewicz@gmail.com>
2020-11-04 07:32:24 +0100

    Smart-Device template
    ver 0.0.0.1

--- smart-home-device-template/Makefile
+++ smart-home-device-template/Makefile
@@ -5,7 +5,11 @@
 
 PROJECT_NAME := smart-home-device-template
 
-EXTRA_COMPONENT_DIRS += ./../smart-home-device-libs/network
+EXTRA_COMPONENT_DIRS += ./../smart-home-device-libs/network_wrapper
+EXTRA_COMPONENT_DIRS += ./../smart-home-device-libs/ota_wrapper
+EXTRA_COMPONENT_DIRS += ./../smart-home-device-libs/mqtt_wrapper
+EXTRA_COMPONENT_DIRS += ./../smart-home-device-libs/utils
+EXTRA_COMPONENT_DIRS += ./../smart-home-device-libs/smart-device
 $(info "EXTRA_COMPONENT_DIRS=" $(EXTRA_COMPONENTS_DIRS))
 
 include $(IDF_PATH)/make/project.mk
--- smart-home-device-template/main/main.cpp
+++ smart-home-device-template/main/main.cpp
@@ -13,73 +13,42 @@
 #include "esp_https_ota.h"
 #include "esp_http_client.h"
 
-#include "Network/WifiStation.hpp"
+//#include "Network/WifiStation.hpp"
 
-#define CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL "https://192.168.1.20:8070/test.bin"
+//#include "SmartDevice/Network.hpp"
+
+#include "SmartDevice/Kernel.hpp"
+
+void * operator new( size_t size )
+{
+    return pvPortMalloc( size );
+}
+
+void * operator new[]( size_t size )
+{
+    return pvPortMalloc(size);
+}
+
+void operator delete( void * ptr )
+{
+    vPortFree ( ptr );
+}
+
+void operator delete[]( void * ptr )
+{
+    vPortFree ( ptr );
+}
 
 //Tag used for logging
 static const char* TAG = "TemplateNode";
 
-extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
-extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
-
 //Make app_main compatible with C++
 extern "C"{ void app_main(void); }
 
-esp_err_t _http_event_handler(esp_http_client_event_t *evt)
-{
-    switch (evt->event_id) {
-    case HTTP_EVENT_ERROR:
-        ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
-        break;
-    case HTTP_EVENT_ON_CONNECTED:
-        ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
-        break;
-    case HTTP_EVENT_HEADER_SENT:
-        ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
-        break;
-    case HTTP_EVENT_ON_HEADER:
-        ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
-        break;
-    case HTTP_EVENT_ON_DATA:
-        ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-        break;
-    case HTTP_EVENT_ON_FINISH:
-        ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
-        break;
-    case HTTP_EVENT_DISCONNECTED:
-        ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
-        break;
-    }
-    return ESP_OK;
-}
-
-void simple_ota_example_task(void *pvParameter)
-{
-    ESP_LOGI(TAG, "Starting OTA example");
-
-    esp_http_client_config_t config;
-
-    config.url = CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL;
-    config.cert_pem = (char *)server_cert_pem_start;
-    config.event_handler = _http_event_handler;
-
-
-    while (1) {
-        esp_err_t ret = esp_https_ota(&config);
-        if (ret == ESP_OK) {
-            ESP_LOGI(TAG,"Firmware upgraded, restarting...");
-            esp_restart();
-        } else {
-            ESP_LOGE(TAG, "Firmware upgrade failed");
-        }
-
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
-}
-
 void app_main(void)
 {
+    esp_log_level_set("*", ESP_LOG_DEBUG);
+
     ESP_LOGD(TAG, "main starting...123");
     esp_log_level_set("*", ESP_LOG_DEBUG);   
 
@@ -91,9 +60,16 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(ret);
 
-    WifiStation wifiStation;
-    wifiStation.start();
-    wifiStation.isConnected(true);
+    SmartDevice::Kernel kernel;
+    kernel.getOtaTask().setBinaryName("test123.bin");
+    kernel.start();
+
+    //SmartDevice::NetworkTask networkTask;
+    //networkTask.start();
+
+    // WifiStation wifiStation;
+    // wifiStation.start();
+    // wifiStation.isConnected(true);
 
     //vTaskDelay(pdMS_TO_TICKS(3000));
 
@@ -111,7 +87,7 @@ void app_main(void)
     //   ESP_LOGW(TAG, "%s", e.what());
     // }
 
-    xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
+    //xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
     
     while(1) { vTaskDelay(1000 / portTICK_PERIOD_MS); }
 }
